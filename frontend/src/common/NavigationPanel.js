b3dc1218fd1c5d3caa309083f0238d8e8edcefee
rakhmukova <lessness19@gmail.com>
2023-05-09 14:40:24 +0200

    Create TopPanel to show app name and user data

--- frontend/src/common/NavigationPanel.js
+++ frontend/src/common/NavigationPanel.js
@@ -1,3 +1,5 @@
+//@flow
+
 import NavigationButton from './NavigationButton'
 import './NavigationPanel.css'
 
---
+++ frontend/src/common/TopPanel.js
@@ -0,0 +1,11 @@
+//@flow
+
+import './TopPanel.css'
+
+export default function TopPanel() {
+    return (
+        <div className="top-panel">
+            <h2 className="app-name">Log Parser</h2>
+        </div>
+    )
+}
