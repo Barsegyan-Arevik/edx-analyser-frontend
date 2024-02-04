937d4df4286968f25da1f05db45c1514ed4d27fc
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-02-04 14:32:56 +0300

    added shimmering

--- frontend/src/common/Charts/BarChart.tsx
+++ frontend/src/common/Charts/BarChart.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
+import { ShimmerThumbnail } from 'react-shimmer-effects'
+import {useEffect, useState} from "react";
+
 
 const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
 const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
@@ -14,17 +17,35 @@ const xLabels = [
 ];
 
 export default function SimpleBarChart() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+      // Имитация задержки загрузки данных
+      const timer = setTimeout(() => {
+          setLoading(false);
+      }, 4000);
+
+      // Очистка таймера при размонтировании компонента
+      return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <BarChart
-      width={500}
-      height={300}
-      colors={['#02CEA9']}
-      series={[
-        { data: pData, label: 'Количество уникальных просмотров', id: 'pvId' },
-      ]}
-      xAxis={[{ data: xLabels, scaleType: 'band'}]}
+      <div style={{ position: 'relative' }}>
+            {loading ? (
+                <ShimmerThumbnail height={250} width={550} rounded />
+            ) : (
+                <BarChart
+                  width={500}
+                  height={300}
+                  colors={['#02CEA9']}
+                  series={[
+                    { data: pData, label: 'Количество уникальных просмотров', id: 'pvId' },
+                  ]}
+                  xAxis={[{ data: xLabels, scaleType: 'band'}]}
 
 
-    />
+                />
+            )}
+      </div>
   );
 }
--- frontend/src/common/Charts/DonutsChart.tsx
+++ frontend/src/common/Charts/DonutsChart.tsx
@@ -1,4 +1,9 @@
 import { PieChart } from '@mui/x-charts'
+import Skeleton from 'react-loading-skeleton'
+import * as React from 'react';
+import { useState, useEffect } from 'react'
+import { ShimmerThumbnail } from 'react-shimmer-effects'
+
 
 export type DonutsChartData = {
     value: number;
@@ -16,25 +21,52 @@ export type DonutsChartProps = {
 //     ];
 
 export default function DonutsChart(props: DonutsChartProps) {
-    return (
-        <PieChart
-            colors={['#5471E7', '#02CEA9', '#FEF045']}
-            series={[
-            {
-              data: props.data,
-              innerRadius: 50,
-              outerRadius: 100,
-              paddingAngle: 1,
-              cornerRadius: 3,
-              startAngle: -180,
-              endAngle: 180,
-              cx: 200,
-              cy: 100,
-            },
-          ]}
+    const { data } = props;
+    const [loading, setLoading] = useState(true);
 
-          width={550}
-          height={250}
-        />
+    useEffect(() => {
+        // Имитация задержки загрузки данных
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 4000);
+
+        // Очистка таймера при размонтировании компонента
+        return () => clearTimeout(timer);
+    }, []);
+
+    return (
+        <div style={{ position: 'relative' }}>
+            {loading ? (
+                <ShimmerThumbnail height={250} width={550} rounded />
+            ) : (
+            <PieChart
+                colors={['#5471E7', '#02CEA9', '#FEF045']}
+                series={[
+                {
+                  data: props.data,
+                  innerRadius: 50,
+                  outerRadius: 100,
+                  paddingAngle: 1,
+                  cornerRadius: 3,
+                  startAngle: -180,
+                  endAngle: 180,
+                  cx: 200,
+                  cy: 100,
+                },
+              ]}
+
+              slotProps={{
+                legend: {
+                  labelStyle: {
+                    fontSize: 14,
+                    fill: "#667B98",
+                  },
+                },
+              }}
+              width={550}
+              height={250}
+            />
+        )}
+        </div>
     );
 };
