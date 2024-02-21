f404a2fd19b90ebc6de147b7b84f38e632f7397d
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-02-21 21:52:52 +0300

    The text is passed to the boxes using properties

--- frontend/src/components/Charts/CustomBox.tsx
+++ frontend/src/components/Charts/CustomBox.tsx
@@ -3,43 +3,65 @@ import {Box, ThemeProvider, createTheme} from '@mui/system';
 
 export type CustomBoxProps = {
     value: string;
+    // value_additional_text: string;
     label: string;
 }
 
 export default function CustomBox(props: CustomBoxProps) {
     return (
-        // <ThemeProvider theme={theme}>
         <Box
             sx={{
                 // boxShadow: '0 4px 6px rgba(50, 50, 93, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)',
-                minWidth: 200,
-                height: 121,
+                // width: 249,
+                // height: 135,
                 bgcolor: '#fff',
                 borderColor: '#F5F5F5',
                 color: '#405479',
                 p: 2,
                 borderRadius: 1,
-                textAlign: 'center',
-                fontSize: 15,
+                textAlign: 'left',
+                // fontSize: 15,
                 fontWeight: 'normal',
             }}
         >
-            {props.label}
             <Box
                 sx={{
-                    bgcolor: 'background.paper',
-                    boxShadow: 1,
-                    borderRadius: 2,
-                    p: 2,
-                    minWidth: 350,
+                    // p: 1,
+                    // paddingBottom: 1,
+                    textAlign: 'left',
+                    color: '#5471E7',
+                    fontSize: 48,
+                    fontWeight: 'medium'
                 }}
             >
-                <Box sx={{color: '#02CEA9', fontSize: 34, fontWeight: 'medium'}}>
-                    {props.value}
-                </Box>
+                {props.value}
 
+                {/*<Box*/}
+                {/*    sx={{*/}
+                {/*        color: '#5471E7',*/}
+                {/*        display: 'inline',*/}
+                {/*        fontWeight: 'bold',*/}
+                {/*        mx: 0.5,*/}
+                {/*        fontSize: 14,*/}
+                {/*    }}*/}
+                {/*>*/}
+                {/*    {props.value_additional_text}*/}
+                {/*</Box>*/}
             </Box>
-            {/*</ThemeProvider>*/}
+            <Box
+                sx={{
+                    textAlign: 'left',
+                    fontSize: 20,
+                    fontWeight: 'normal',
+                    // p: 1,
+                }}
+            >
+                {props.label}
+            </Box>
         </Box>
     );
 }
+
+
+
+
