eddb93373a04d5a891ea1ae1ad8f1841832a027d
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-02-21 22:01:28 +0300

    Added CSS styles

--- frontend/src/components/unionParts/TopSection/TopAnalytics.tsx
+++ frontend/src/components/unionParts/TopSection/TopAnalytics.tsx
@@ -1,29 +1,36 @@
 import * as React from 'react';
-import DonutsChart from "../Charts/DonutsChart";
-import CustomBox from "../Charts/CustomBox";
+import DonutsChart from "../../Charts/DonutsChart";
+import CustomBox from "../../Charts/CustomBox";
 import './TopAnalytics.css'
+import Header from "../HeaderSection/Header"
 
-export default function TopAnalytics(){
+
+export default function TopAnalytics() {
     const chartDonut = [
         {value: 10, label: 'Прошли курс'},
         {value: 20, label: 'Начали, не прошли'},
         {value: 15, label: 'Не начали'},
     ]
 
-    const aboutCountOfStudents = {value: '864', label: 'Количество студентов, записавшихся на курс:'}
-    const averageTimeToEnroll = {value: '10:54:44', label: 'Среднее время регистрации на курс:'}
+    const mainHeaderText = "Аналитика по курсу \"Название курса\""
+    const aboutCountOfStudents = {value: '864', label: 'Количество студентов, записавшихся на курс'}
+    const averageTimeToEnroll = {value: '10:54:44', label: 'Среднее время регистрации на курс'}
     return (
-        <div className={"top_analytics"} >
-            <div className={"main-box"}>
-                <DonutsChart data={chartDonut}/>
-            </div>
+        <div className={"top_analytics"}>
 
-            <div className={"side-container"}>
-                <div className={"side-box-up"}>
-                    <CustomBox value={aboutCountOfStudents.value} label={aboutCountOfStudents.label}/>
+            <Header text={mainHeaderText}/>
+            <div className={"main_contentg"}>
+                <div className={"main-box"}>
+                    <DonutsChart data={chartDonut}/>
                 </div>
-                <div className={"side-box-down"}>
-                    <CustomBox value={averageTimeToEnroll.value} label={averageTimeToEnroll.label} />
+
+                <div className={"side-container"}>
+                    <div className={"side-box-up"}>
+                        <CustomBox value={aboutCountOfStudents.value} label={aboutCountOfStudents.label}/>
+                    </div>
+                    <div className={"side-box-down"}>
+                        <CustomBox value={averageTimeToEnroll.value} label={averageTimeToEnroll.label}/>
+                    </div>
                 </div>
             </div>
         </div>
