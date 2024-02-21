fd4b5b8ae3062017adb4aa3b796a4aa73e59d11b
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-02-21 22:08:12 +0300

    Small change

--- frontend/src/components/PageBase/PageBase.tsx
+++ frontend/src/components/PageBase/PageBase.tsx
@@ -10,12 +10,6 @@ export default function PageBase(props: PageBaseProps) {
     const windowHeight = window.innerHeight;
     return (
         <div className="page-base fullscreen">
-            <div style={{height: windowHeight}}></div>
-            <div className={"bg_image"}>
-                {/*<img src="./Rectangle.svg"/>*/}
-                Привет, тут текст
-            </div>
-            <div style={{height: windowHeight}}></div>
             <div className="content">
                 {props.children}
             </div>
--- frontend/src/components/unionParts/DocumentSection/DocumentInteraction.tsx
+++ frontend/src/components/unionParts/DocumentSection/DocumentInteraction.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import './DocumentInteraction.css'
-import MinMaxExample from "../Charts/ScatterWithSlider";
+import MinMaxExample from "../../Charts/ScatterWithSlider";
 
 export default function DocumentInteraction(){
     return (
