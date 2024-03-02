57910db902089adcb253a4b15f9c5c4bdb3590e2
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-03-02 20:40:18 +0300

    Added parametrization

--- frontend/src/pages/CourseInfoPage.tsx
+++ frontend/src/pages/CourseInfoPage.tsx
@@ -1,48 +1,38 @@
-import * as React from 'react';
+import React from 'react';
 import PageBase from "../components/PageBase/PageBase";
-import TopAnalytics from "../components/unionParts/TopSection/TopAnalytics";
-import DocumentInteraction from "../components/unionParts/DocumentSection/DocumentInteraction";
-import VideoInteractions from "../components/unionParts/VideoSection/VideoInteractions";
+import CourseAnalytics from "../components/unionParts/CourseAnalytics/CourseAnalytics";
+import DocumentSection from "../components/unionParts/DocumentSection/DocumentSection";
+import VideoSection from "../components/unionParts/VideoSection/VideoSection";
 import './CourseInfoPage.css'
-import Header from "../components/unionParts/HeaderSection/Header";
-import BarAnimation from "../components/Charts/BarChartWithSlider";
-import CustomTable from "../components/Charts/TableHeatMap";
-import {BarChart} from '@mui/x-charts/BarChart';
-import {Box} from '@mui/material';
-import {blue} from "@mui/material/colors";
-import BarChartWithModalWindow from "../components/Charts/BarChartWithModalWindow";
+import { CourseAnalyticsProps } from '../components/unionParts/CourseAnalytics/CourseAnalytics';
+import { VideoSectionProps } from '../components/unionParts/VideoSection/VideoSection';
+import { DocumentSectionProps } from '../components/unionParts/DocumentSection/DocumentSection';
 
-export default function CourseInfoPage() {
-    const chartDonut = [
-        {value: 10, label: 'Прошли курс'},
-        {value: 20, label: 'Начали, не прошли'},
-        {value: 15, label: 'Не начали'},
-    ]
 
-    const generateSampleData = (count) => {
-        const data = [];
-        for (let i = 1; i <= count; i++) {
-            data.push({
-                name: `User ${i}`,
-                score: Math.floor(Math.random() * 101), // Random score between 0 and 100
-            });
-        }
-        return data;
-    };
+export type CourseInfoPageProps = {
+    courseAnalyticsProps: CourseAnalyticsProps;
+    videoSectionProps: VideoSectionProps;
+    documentSectionProps: DocumentSectionProps;
+}
 
+export default function CourseInfoPage(props: CourseInfoPageProps) {
+    
     const windowHeight = window.innerHeight;
     return (
         <PageBase>
             <div className={"layout"}>
-                <div className={"content"} style={{height: '77vh', color: '#405479'}}>
-                    <TopAnalytics/>
-                </div>
-                <div className={"bg_image"}>
-                    <div className={"content"}>
-                        <VideoInteractions/>
-                        <BarAnimation/>
-                        <CustomTable/>
-                        <BarChartWithModalWindow/>
+                {/* <div className={"content"} style={{height: '77vh', color: '#405479'}}> */}
+                <div style={{justifyContent: 'center'}}>
+                    <div style={{ height: '85vh', color: '#405479' }}>
+                        <CourseAnalytics {...props.courseAnalyticsProps}/>
+                    </div>
+                    <div className={"spikes"}>
+                        <div className='wrapper'>
+                            <VideoSection {...props.videoSectionProps}/>
+                        </div>
+                    </div>
+                    <div style={{ height: '85vh'}}>
+                        <DocumentSection {...props.documentSectionProps}/>
                     </div>
                 </div>
             </div>
