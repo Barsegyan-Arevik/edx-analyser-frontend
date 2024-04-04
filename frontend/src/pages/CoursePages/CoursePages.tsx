cafc09a92ca3d2d64ff11ad02e65fa079a65655e
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-04 15:35:11 +0300

    Added files that will contain content for the submenu in the future

---
+++ frontend/src/pages/CoursePages/CoursePages.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import PageBase from "../../components/PageBase/PageBase";
+import CustomTable from "../../components/Charts/Table/SimpleTable";
+import './CoursePages.css'
+export default function CoursePages() {
+
+    return (
+        <PageBase>
+            Популярность страниц курса
+            {/*<CustomBox />*/}
+            <footer className="footer">
+                {/* Содержимое вашего футера */}
+            </footer>
+        </PageBase>
+    );
+};
