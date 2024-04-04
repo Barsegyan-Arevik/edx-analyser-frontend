12c00b7c7a7fca01b18220a4fbf8f552dd7296ac
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-04 21:49:56 +0300

    The color scale extraction functions have been moved here

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -41,4 +41,38 @@ export function getColorCompletionStatus(status) {
     }
 }
 
+export function lerp(a: number, b: number, t: number) {
+    return a + (b - a) * t;
+}
 
+export function getGreenColorScale(range: number, value: number) {
+    const ratio = value / range; // Max population value
+    const brightestColor = [2, 206, 169]; // R, G, B values of bright green
+    const paleColor = [240, 240, 240]; // R, G, B values of pale green
+    const color = brightestColor.map((channel, index) =>
+        Math.round(lerp(paleColor[index], channel, ratio))
+    );
+    return `rgb(${color.join(',')})`;
+}
+
+export function getBlueColorScale(timeRange: number, minValue: number, value: number) {
+    value = value - minValue;
+    const ratio = value / (timeRange); // Max population value
+
+    const purpleColor = [163, 213, 255];
+    const mediumBlueColor = [204, 232, 255];
+    const paleBlueColor = [217, 240, 255];
+
+    let color: number[];
+
+    if (value <= 0.5) {
+        color = paleBlueColor.map((channel, index) =>
+            Math.round(lerp(channel, mediumBlueColor[index], ratio))
+        );
+    } else {
+        color = mediumBlueColor.map((channel, index) =>
+            Math.round(lerp(channel, purpleColor[index], ratio))
+        );
+    }
+    return `rgb(${color.join(',')})`;
+}
