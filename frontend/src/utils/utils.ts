060c477dacdb4754dea3697d2c5d0cef7991a9ba
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-18 14:53:03 +0300

    Experimented with the getGreenColorScale function

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -17,13 +17,34 @@ export function lerp(a: number, b: number, t: number) {
     return a + (b - a) * t;
 }
 
-export function getGreenColorScale(range: number, value: number) {
-    const ratio = value / range; // Max population value
-    const brightestColor = [2, 206, 169]; // R, G, B values of bright green
-    const paleColor = [240, 240, 240]; // R, G, B values of pale green
-    const color = brightestColor.map((channel, index) =>
-        Math.round(lerp(paleColor[index], channel, ratio))
-    );
+export function getGreenColorScale(range: number, minValue: number, value: number) {
+    // const ratio = value / range; // Max population value
+    // const brightestColor = [2, 206, 169];
+    // const mediumColor = [147, 225, 217]
+    // const paleColor = [221, 255, 247];
+    // const color = brightestColor.map((channel, index) =>
+    //     Math.round(lerp(paleColor[index], channel, ratio))
+    // );
+    // return `rgb(${color.join(',')})`;
+    //
+    value = value - minValue;
+    const ratio = value / (range); // Max population value
+
+    const purpleColor = [2, 206, 169];
+    const mediumBlueColor = [131, 197, 190];
+    const paleBlueColor = [237, 246, 249];
+
+    let color: number[];
+
+    if (value <= 0.5) {
+        color = paleBlueColor.map((channel, index) =>
+            Math.round(lerp(channel, mediumBlueColor[index], ratio))
+        );
+    } else {
+        color = mediumBlueColor.map((channel, index) =>
+            Math.round(lerp(channel, purpleColor[index], ratio))
+        );
+    }
     return `rgb(${color.join(',')})`;
 }
 
