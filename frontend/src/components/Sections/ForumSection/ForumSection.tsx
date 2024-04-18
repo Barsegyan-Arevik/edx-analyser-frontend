5a2344ad0d6693ca187e802cb792aa11392025ee
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-18 13:36:50 +0300

    The Forum section has been added

---
+++ frontend/src/components/Sections/ForumSection/ForumSection.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import SectionHeader from '../SectionHeader/SectionHeader'
+import ForumActivityChart from '../../Charts/ForumActivityChart/ForumActivityChart';
+import {ForumReport} from '../../../models/report';
+
+
+export type ForumSectionProps = {
+    report: ForumReport;
+}
+
+export default function ForumSection(props: ForumSectionProps) {
+    return (
+        <div>
+            <SectionHeader text={'Активность на форуме'}/>
+            <ForumActivityChart chart={props.report.forum_question_chart.items} />
+        </div>
+    )
+}
