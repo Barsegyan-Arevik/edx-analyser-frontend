7655016b7f066dee6047227e2707cbade439898f
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-04-18 14:50:51 +0300

    A chart of the search by word is drawn

--- frontend/src/components/Sections/TextbookSection/TextbookSection.tsx
+++ frontend/src/components/Sections/TextbookSection/TextbookSection.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import './TextbookSection.css'
 import { TextbookReport } from '../../../models/report'
+import ChartWrapper from '../../Charts/ChartWrapper/ChartWrapper';
+import TableWithLinkAndSearchBar, {RowData} from '../../Charts/Table/TableWithLinkAndSearchBar';
+import TableWithLinkAndSearchBarInsideWindow from '../../Charts/Table/TableWithLinkAndSearchBarInsideWindow';
+import {WordSearchCount} from '../../../models/textbook';
+import SectionHeader from '../SectionHeader/SectionHeader';
 
 export type TableData = {
     columnName: string;
@@ -10,15 +15,56 @@ export type TableData = {
     data: string;
 }
 
+function transformData(data: WordSearchCount[]): RowData[] {
+    return data.map(problem => ({
+        value: problem.word,
+        count: problem.search_count,
+    }));
+}
+
 export type TextbookSectionProps = {
     report: TextbookReport
 }
 
-export default function TextbookSection(props: TextbookSectionProps) {
 
+
+export default function TextbookSection(props: TextbookSectionProps) {
+    const boxTitleSearchedTerms = 'Поиск по слову в учебнике'
+    const columnNameSearchedTerms = 'Слова';
+    const columnCountSearchedTerms = 'Количество поисков';
+    const labelTextSearchedTerms = 'Поиск слова...';
+    const transformedData = transformData(props.report.word_search_chart.items);
+    console.log('im here')
     return (
-        <div className={'document_interaction'}>
-            {props.report.course_id}
+        <div>
+            {/*{props.report.course_id}*/}
+            <SectionHeader text={'Работа с учебником'} />
+            <ChartWrapper
+                chartTitle={boxTitleSearchedTerms}
+                chart={
+                    <TableWithLinkAndSearchBar
+                        rows={{items: transformedData}}
+                        columnName={columnNameSearchedTerms}
+                        columnCount={columnCountSearchedTerms}
+                        labelText={labelTextSearchedTerms}
+                    />}
+                popupChart={
+                    <TableWithLinkAndSearchBarInsideWindow
+                        boxTitle={boxTitleSearchedTerms}
+                        rows={{items: transformedData}}
+                        columnName={columnNameSearchedTerms}
+                        columnCount={columnCountSearchedTerms}
+                        labelText={labelTextSearchedTerms}
+                    />
+                }
+                additionalInfo={''}
+            />
+
         </div>
     )
 }
+
+
+
+
+
