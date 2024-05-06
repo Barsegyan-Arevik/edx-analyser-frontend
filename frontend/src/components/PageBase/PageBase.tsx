6bf285be40fc7c69d2940dcf9997236ea121b75b
Barsegyan-Arevik <arevikbarsegan5@gmail.com>
2024-05-06 09:53:43 +0300

    Experiments with footer

--- frontend/src/components/PageBase/PageBase.tsx
+++ frontend/src/components/PageBase/PageBase.tsx
@@ -13,6 +13,14 @@ export default function PageBase(props: PageBaseProps) {
                 {props.children}
             </div>
             {/*<footer className="footer"/>*/}
+            {/*<footer className='footer'>*/}
+            {/*    <div className="waves">*/}
+            {/*        <div className="wave" id="wave1"></div>*/}
+            {/*        <div className="wave" id="wave2"></div>*/}
+            {/*        <div className="wave" id="wave3"></div>*/}
+            {/*        <div className="wave" id="wave4"></div>*/}
+            {/*    </div>*/}
+            {/*</footer>*/}
         </div>
     )
 }
