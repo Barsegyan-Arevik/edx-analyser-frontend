6c2e284fb7b6d4f06680e459ce323734f3712ceb
rakhmukova <lessness19@gmail.com>
2024-05-14 13:45:56 +0300

    Update course form with new fields

--- frontend/src/components/courses/CreateCourseForm.tsx
+++ frontend/src/components/courses/CreateCourseForm.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {useState} from 'react';
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid} from '@mui/material';
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, Grid, Switch} from '@mui/material';
 import FileUploadForm from './FileUploadForm';
 import TextField from '@mui/material/TextField';
 import {BASE_URL} from '../../config';
@@ -13,6 +13,8 @@ type CreateCourseFormProps = {
 
 export default function CreateCourseForm(props: CreateCourseFormProps) {
     const [selectedFile, setSelectedFile] = useState(null);
+    const [isPublic, setIsPublic] = useState(false);
+    const [name, setName] = useState('')
 
     const handleUpload = () => {
         const formData = new FormData();
@@ -39,12 +41,19 @@ export default function CreateCourseForm(props: CreateCourseFormProps) {
             <DialogTitle>Добавить курс</DialogTitle>
             <DialogContent>
                 <form onSubmit={handleSubmit}>
-                    <Grid container spacing={2}>
+                    <Grid container spacing={2} paddingTop={2}>
                         <Grid item xs={12}>
-                            <TextField name="courseName" label="Название курса" fullWidth required />
+                            <TextField name="courseName" label="Название курса" fullWidth onChange={e => setName(e.target.value)} required/>
                         </Grid>
-                        <Grid item xs={12}>
-                            <FileUploadForm selectedFile={selectedFile} onFileChange={setSelectedFile} />
+                        <Grid item xs={7}>
+                            <FileUploadForm selectedFile={selectedFile} onFileChange={setSelectedFile}/>
+                        </Grid>
+                        <Grid container xs={5} justifyContent="flex-end">
+                            <FormControlLabel
+                                label="Доступен всем"
+                                labelPlacement={'end'}
+                                control={<Switch checked={isPublic} onChange={() => setIsPublic(!isPublic)}/>}
+                            />
                         </Grid>
                     </Grid>
                     <DialogActions>
